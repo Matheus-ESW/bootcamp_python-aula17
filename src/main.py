7b5d1c655d0c1529c765328dfa969ceca2d808f9
Matheus Ramos <matheussoaresramos@gmail.com>
2025-11-18 14:27:32 -0300

    Cria engine e sqlite database, table users

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# Conectar ao SQLite em memória
+engine = create_engine('sqlite:///meubanco.db', echo=True)
+
+print("Conexão com SQLite estabelecida.")
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+
+# Criar as tabelas no banco de dados
+Base.metadata.create_all(engine)
+
